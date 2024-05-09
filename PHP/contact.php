00c2cbf0ce5a7cb837458291ad0508f012bf42c6
chloebaisse1 <baissechloe@gmail.com>
2024-05-09 15:50:05 +0400

    supp et ajout nouvelles img ajout fichier PHP

---
+++ PHP/contact.php
@@ -0,0 +1,6 @@
+<form action="/contact.php" method="post">
+  <input type="text" name="titre" placeholder="Titre">
+  <input type="text" name="description">
+  <input type="text" value="Email" placeholder="email">
+  <button type="submit">Envoyer</button>
+</form>
